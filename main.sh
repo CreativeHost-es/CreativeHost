57b32ae12eb1f2e9fba775eb34fd757c085b7815
Dectectable <75132173+Dectectable@users.noreply.github.com>
2023-02-08 14:10:37 -0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,9 +3,6 @@ if [ "$(id -u)" != "0" ]; then
    exit 1
 fi
 
-apt install sudo -i
-
-
 echo "#######################################################################################"
 echo "#"
 echo "#                                  CREATIVEHOST.ES SCRIPTS"
@@ -15,16 +12,29 @@ echo "#"
 echo "#"
 echo "#######################################################################################"
 echo ""
+echo "* [0] Install Updates Ubuntu"
 echo "* [1] Install Warp to enable Ipv4 on Ipv6 only"
-echo "* [2] Exit"
+echo "* [2] Get our Discord Now"
+echo "* [3] Exit"
 
 read -p "Enter Input (1-2): " input
 
 case $input in
-
+     
     0)
+    apt update && apt upgrade -y
+    1)
     apt update
     apt install curl -y
     wget -N https://raw.githubusercontent.com/Dectectable/creativehost/main/scripts/warpv6/install.sh && bash install.sh;;
-    1) echo "Exit" && exit;;
+    2)
+    echo "#######################################################################################"
+    echo "#"
+    echo "#                                  https://discord.gg/GKev8uZQBz"
+    echo "#"
+    echo "#                           Copyright (C) 2022 - 2023, CREATIVEHOST.ES"
+    echo "#"
+    echo "#"
+    echo "#######################################################################################"
+    3) echo "Exit" && exit;;
 esac
